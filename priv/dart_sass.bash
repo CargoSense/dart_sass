d14af62cd8a7a73d8c5c2170bc957263fb86538e
Michael Crumm <michael.crumm@cargosense.com>
2021-07-30 10:21:49 -0700

    Add comments about purpose to the bash script

--- priv/dart_sass.bash
+++ priv/dart_sass.bash
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# This script was taken from the Elixir Port guide. It is used to ensure
+# graceful termination of the `sass` process when it detects that stdin
+# has been closed.
+# Link: https://hexdocs.pm/elixir/Port.html#module-zombie-operating-system-processes
+#
+# This script is required until dart-sass supports listening on stdin and
+# gracefully terminating when stdin is closed. There is currently a PR for
+# this behaviour: https://github.com/sass/dart-sass/pull/1411
+#
 # Start the program in the background
 exec "$@" &
 pid1=$!
