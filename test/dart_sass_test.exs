45531d369363212555cfd7416cc8b1ee5ede5504
Michael Crumm <michael.crumm@cargosense.com>
2021-08-23 18:48:10 -0700

    Update version in test assertions

--- test/dart_sass_test.exs
+++ test/dart_sass_test.exs
@@ -4,12 +4,12 @@ defmodule DartSassTest do
   test "run on default" do
     assert ExUnit.CaptureIO.capture_io(fn ->
              assert DartSass.run(:default, ["--version"]) == 0
-           end) =~ "1.36.0"
+           end) =~ "1.38.1"
   end
 
   test "run on profile" do
     assert ExUnit.CaptureIO.capture_io(fn ->
              assert DartSass.run(:another, []) == 0
-           end) =~ "1.36.0"
+           end) =~ "1.38.1"
   end
 end
